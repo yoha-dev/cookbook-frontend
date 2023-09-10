7870578c87b02258ee13c7ff89643d80dd84ae2f
yoha-dev <yoha-dev@mailbox.org>
2023-09-10 21:46:09 +0200

    add recipe creation poc

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Button from "primevue/button"
+
+import '@/assets/main.css'
+import 'primevue/resources/themes/lara-light-teal/theme.css';
+import 'primevue/resources/primevue.min.css';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
--- src/views/CreateRecipe.vue
+++ src/views/CreateRecipe.vue
@@ -1,9 +1,53 @@
 <script setup lang="ts">
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import Textarea from 'primevue/textarea';
+import MultiSelect from 'primevue/multiselect';
+import { ref } from 'vue';
+
+
+const title = ref('')
+const description = ref('')
+
+const genres = ref(['curry', 'salad', 'Suppe'])
+
+const selectedGenres = ref([])
+
+function send(){
+  console.log('title+description', JSON.stringify( {
+      title, 
+      description,
+      genres
+    }))
+  fetch('http://localhost:3000/recipes', {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify( {
+      "title": title.value, 
+      "description": description.value,
+      "genres": selectedGenres.value,
+    }),
+  })
+}
+
 </script>
 
 <template>
   <main>
     <h1>Rezept erstellen</h1>
+    <h3>Titel</h3>
+    <InputText type="text" v-model="title" placeholder="Spaghetti Bolognese" />
+    <h3>Beschreibung</h3>
+    <Textarea v-model="description" rows="5" cols="30" placeholder="Beschreibung"/>
+    <br/>
+    <MultiSelect v-model="selectedGenres" :options="genres" placeholder="Genres wählen"
+    :maxSelectedLabels="3" display="chip"/>
+    <br/>
+    <Button @click="send" icon="pi pi-check" iconPos="right" raised label="Submit" />
   </main>
 </template>
 
