fe706b2a84765d8197a976146caf583236288006
yoha-dev <yoha-dev@mailbox.org>
2023-09-10 18:43:16 +0200

    initial commit

---
+++ src/env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+    readonly VITE_APP_TITLE: string
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
---
+++ src/views/CreateRecipe.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Rezept erstellen</h1>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
